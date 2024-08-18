da871cac0da0e8abb2ac44fbb1d52bbcac252af5
Reza Radfar <reza.radfar1306@gmail.com>
2024-08-19 02:49:34 +0330

    comletes js for slidebar in testimonials

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log(" index works");
+
+const slides = document.querySelectorAll(".testimonials__box");
+const slidesCount = slides.length;
+let currSlide = 0;
+goToSlide(0);
+
+function goToSlide(number) {
+  slides.forEach((s, i) => {
+    if (i !== number)
+      s.style.transform = `translate(${
+        (i - number) * 100 - 50
+      }%, -50%) scale(0.6)`;
+    else
+      s.style.transform = `translate(${
+        (i - number) * 100 - 50
+      }%, -50%) scale(1)`;
+  });
+}
+
+const lArrow = document.querySelector(".testimonials__arrow--left");
+const rArrow = document.querySelector(".testimonials__arrow--right");
+
+console.log(lArrow);
+
+rArrow.addEventListener("click", function () {
+  currSlide++;
+  if (currSlide > slidesCount - 1) currSlide = 0;
+
+  goToSlide(currSlide);
+});
+
+lArrow.addEventListener("click", function () {
+  currSlide--;
+  if (currSlide < 0) currSlide = slidesCount - 1;
+
+  goToSlide(currSlide);
+});
